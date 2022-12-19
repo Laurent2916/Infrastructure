9175ce34ca949cf80f567f426a72e0f5da74abbe
Laureηt <laurent@fainsin.bzh>
2022-12-19 23:58:32 +0100

    chore: cleaning + add packages

--- hosts/neodymium/configuration.nix
+++ hosts/neodymium/configuration.nix
@@ -19,55 +19,27 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  networking.hostName = "neodymium"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Paris";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_IE.UTF-8";
   console = {
-  #   font = "Lat2-Terminus16";
     keyMap = "fr";
-  #   useXkbConfig = true; # use xkbOptions in tty.
   };
 
   fonts.fonts = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   services.dbus.enable = true;
   xdg.portal = {
@@ -91,61 +63,42 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "video" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
+      git
+      exa
+      bat
+      tmux
+      htop
+      rsync
+      pwgen
       neovim
-      keepassxc
-      librewolf
-
-      pavucontrol
-      baobab
-      element-web
-      sway
+      
+      mpv
+      feh
       vscode
-      waybar
+      librewolf
+      keepassxc
+      x2goclient
+      element-desktop
+
+      baobab
       alacritty
+      pavucontrol
+
+      sway
       wayland
-      glib
       swaylock
       swayidle
-      grim
-      slurp
       wl-clipboard
+      waybar
+      slurp
+      grim
       mako
     ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    htop
-    git
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Enable polkit
   security.polkit.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
