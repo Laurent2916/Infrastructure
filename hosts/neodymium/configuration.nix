d21ca86d423e989c689260aba6db5ac96b4a2bf2
Laureηt <laurent@fainsin.bzh>
2022-12-20 12:40:34 +0100

    feat: starting to use home-manager

--- hosts/neodymium/configuration.nix
+++ hosts/neodymium/configuration.nix
@@ -57,13 +57,15 @@
 
   programs.light.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.mutableUsers = false;
   users.users.laurent = {
-    initialPassword = "laurent";
     isNormalUser = true;
-    extraGroups = [ "wheel" "video" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      git
+    initialPassword = "laurent";
+    extraGroups = [ "wheel" "video" ];
+  };
+  home-manager.users.laurent = {
+    home.stateVersion = "22.11";
+    home.packages = with pkgs; [
       exa
       bat
       tmux
@@ -72,6 +74,18 @@
       pwgen
       neovim
       
+      baobab
+      alacritty
+      pavucontrol
+
+      swaylock
+      swayidle
+      wl-clipboard
+      waybar
+      slurp
+      grim
+      mako
+
       mpv
       feh
       vscode
@@ -80,24 +94,102 @@
       x2goclient
       thunderbird
       element-desktop
-
-      baobab
-      alacritty
-      pavucontrol
-
-      sway
-      wayland
-      swaylock
-      swayidle
-      wl-clipboard
-      waybar
-      slurp
-      grim
-      mako
     ];
-  };
 
-  users.mutableUsers = false;
+    programs.git = {
+      enable = true;
+      userName = "Laureηt";
+      userEmail = "laurent@fainsin.bzh";
+      signing = {
+        signByDefault = true;
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTvwXCT99s1EwOCeGQ28jyCAH/RBoLZza9k5I7wWdEu laurent@neodymium";
+      };
+    };
+
+    wayland.windowManager.sway = {
+      enable = true;
+      config = rec {
+        modifier = "Mod4";
+        terminal = "alacritty";
+        input = {
+          "type:touchpad" = {
+            dwt = "disable";
+            tap = "enabled";
+            natural_scroll = "disabled";
+            middle_emulation = "enabled";
+          };
+          "type:keyboard" = {
+            xkb_numlock = "enabled";
+            xkb_layout = "fr";
+          };
+        };
+        output = {
+          "*" = {
+            bg = "~/Pictures/wallpapers/kai-oberhauser-unsplash.jpg fill";
+          };
+        };
+        bars = [
+          {command = "${pkgs.waybar}/bin/waybar";}
+        ];
+        keybindings = {
+          # Start a terminal
+          "${modifier}+Return" = "exec ${terminal}";
+          # Switch to workspace
+          "${modifier}+ampersand" = "workspace 1";
+          "${modifier}+eacute" = "workspace 2";
+          "${modifier}+quotedbl" = "workspace 3";
+          "${modifier}+apostrophe" = "workspace 4";
+          "${modifier}+parenleft" = "workspace 5";
+          "${modifier}+minus" = "workspace 6";
+          "${modifier}+egrave" = "workspace 7";
+          "${modifier}+underscore" = "workspace 8";
+          "${modifier}+ccedilla" = "workspace 9";
+          "${modifier}+agrave" = "workspace 10";
+          "${modifier}+KP_1" = "workspace 11";
+          "${modifier}+KP_2" = "workspace 12";
+          "${modifier}+KP_3" = "workspace 13";
+          "${modifier}+KP_4" = "workspace 14";
+          "${modifier}+KP_5" = "workspace 15";
+          "${modifier}+KP_6" = "workspace 16";
+          "${modifier}+KP_7" = "workspace 17";
+          "${modifier}+KP_8" = "workspace 18";
+          "${modifier}+KP_9" = "workspace 19";
+          "${modifier}+KP_0" = "workspace 20";
+          "${modifier}+KP_End" = "workspace 11";
+          "${modifier}+KP_Down" = "workspace 12";
+          "${modifier}+KP_Next" = "workspace 13";
+          "${modifier}+KP_Left" = "workspace 14";
+          "${modifier}+KP_Begin" = "workspace 15";
+          "${modifier}+KP_Right" = "workspace 16";
+          "${modifier}+KP_Home" = "workspace 17";
+          "${modifier}+KP_Up" = "workspace 18";
+          "${modifier}+KP_Prior" = "workspace 19";
+          "${modifier}+KP_Insert" = "workspace 20";
+          # Move focused container to workspace
+          "${modifier}+Shift+ampersand" = "move container to workspace number 1";
+          "${modifier}+Shift+eacute" = "move container to workspace number 2";
+          "${modifier}+Shift+quotedbl" = "move container to workspace number 3";
+          "${modifier}+Shift+apostrophe" = "move container to workspace number 4";
+          "${modifier}+Shift+parenleft" = "move container to workspace number 5";
+          "${modifier}+Shift+minus" = "move container to workspace number 6";
+          "${modifier}+Shift+egrave" = "move container to workspace number 7";
+          "${modifier}+Shift+underscore" = "move container to workspace number 8";
+          "${modifier}+Shift+ccedilla" = "move container to workspace number 9";
+          "${modifier}+Shift+agrave" = "move container to workspace number 10";
+          "${modifier}+Shift+KP_End" = "move container to workspace number 11";
+          "${modifier}+Shift+KP_Down" = "move container to workspace number 12";
+          "${modifier}+Shift+KP_Next" = "move container to workspace number 13";
+          "${modifier}+Shift+KP_Left" = "move container to workspace number 14";
+          "${modifier}+Shift+KP_Begin" = "move container to workspace number 15";
+          "${modifier}+Shift+KP_Right" = "move container to workspace number 16";
+          "${modifier}+Shift+KP_Home" = "move container to workspace number 17";
+          "${modifier}+Shift+KP_Up" = "move container to workspace number 18";
+          "${modifier}+Shift+KP_Prior" = "move container to workspace number 19";
+          "${modifier}+Shift+KP_Insert" = "move container to workspace number 20";
+        };
+      };
+    };
+  };
 
   programs.ssh.startAgent = true;
 
@@ -112,4 +204,3 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 }
-
