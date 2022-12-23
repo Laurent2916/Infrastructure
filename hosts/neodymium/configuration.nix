334c7e0cac450d0323f6eb41c5dfbaf03f081326
Laureηt <laurent@fainsin.bzh>
2022-12-23 16:30:53 +0100

    feat: librewolf always restore tabs

--- hosts/neodymium/configuration.nix
+++ hosts/neodymium/configuration.nix
@@ -237,9 +237,10 @@ in {
         "privacy.clearOnShutdown.history" = false;
         "privacy.clearOnShutdown.downloads" = false;
         "privacy.donottrackheader.enabled" = true;
-        "browser.ctrlTab.sortByRecentlyUsed" = true;
+        "browser.ctrlTab.sortByRecentlyUsed" = true; # change Ctrl+Tab behaviour
         "general.autoScroll" = true;
-        "pdfjs.viewerCssTheme" = 2;
+        "pdfjs.viewerCssTheme" = 2; # dark theme for pdf viewer
+        "browser.startup.page" = 3; # always restore tabs
       };
     };
 
