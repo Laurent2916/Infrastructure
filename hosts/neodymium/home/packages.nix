71c53964330b105d53bcadb12cd79e051a9fdc7e
Laureηt <laurent@fainsin.bzh>
2023-04-23 17:21:05 +0200

    💄 sort home packages + add logseq

--- hosts/neodymium/home/packages.nix
+++ hosts/neodymium/home/packages.nix
@@ -2,41 +2,34 @@
   nixpkgs.config.allowUnfree = false;
   services.mpris-proxy.enable = true;
   home.packages = with pkgs; [
-    duf
-    tmux
-    htop
-    rsync
-    pwgen
-    neovim
-
+    baobab # disk utils
+    borgbackup # backup
     dconf
-    xdg-utils
-
-    baobab
-    pavucontrol
-
-    grim
-    mako
-    slurp
-    waybar
-    pamixer
-    swayidle
-    swaylock
-    wl-clipboard
-
-    nixfmt
-
-    borgbackup
-
-    gnome.nautilus
+    duf # replacement for df
+    element-desktop # matrix client
+    feh # image viewer
+    gnome.nautilus # file explorer
+    grim # screenshot utils
+    htop # replacement for top
     jmtpfs
-
-    mpv
-    feh
-    keepassxc
-    x2goclient
-    thunderbird
-    element-desktop
-    libreoffice-fresh
+    keepassxc # password manager
+    libreoffice-fresh # office suite
+    logseq # second brain journalling
+    mpv # video viewer
+    neovim # text editor
+    nixfmt # nix formatter
+    pamixer # volume mixer cli
+    pavucontrol # volume mixer
+    pwgen # password generator
+    rsync # better scp
+    slurp # region selector
+    swayidle # wayland idle hooks
+    swaylock # wayland lockscreen
+    thunderbird # mail client
+    tmux # terminal multiplexer
+    waybar # wayland status bar
+    wl-clipboard # clipboard utils
+    x2goclient # remote desktop
+    xdg-utils
   ];
 }
