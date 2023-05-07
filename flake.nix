a8add62d21cd993143cbebc81e422afe0752fe91
Laureηt <laurent@fainsin.bzh>
2023-05-07 23:11:34 +0200

    ⬆️ nix flake update

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "git+file:///home/laurent/Documents/nixpkgs?shallow=1";
     flake-utils.url = "github:numtide/flake-utils";
 
     lanzaboote = {
