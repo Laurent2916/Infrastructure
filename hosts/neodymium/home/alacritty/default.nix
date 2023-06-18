b84896817631bdc55a2c9d00edeede27c37440bd
Laureηt <laurent@fainsin.bzh>
2023-06-18 14:54:26 +0200

    🔧 change alacritty opacity

--- hosts/neodymium/home/alacritty/default.nix
+++ hosts/neodymium/home/alacritty/default.nix
@@ -11,7 +11,7 @@ in {
     enable = true;
     settings = {
       import = [ "${catppuccin-alacritty}/catppuccin-mocha.yml" ];
-      window.opacity = 0.6;
+      window.opacity = 0.85;
       font = {
         normal.family = "FiraCode Nerd Font Mono";
         size = 9.5;
