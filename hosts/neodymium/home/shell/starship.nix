1ebc992f58bd04585c0acfb2588fc0e688974799
Laureηt <laurent@fainsin.bzh>
2023-06-18 14:55:50 +0200

    ⚡️ (neodymium/shell) switch from p10k to starship

---
+++ hosts/neodymium/home/shell/starship.nix
@@ -0,0 +1,15 @@
+{ lib, ... }: {
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+
+    settings = {
+      format = lib.concatStrings [
+        "$line_break"
+        "$package"
+        "$line_break"
+        "$character"
+      ];
+    };
+  };
+}
