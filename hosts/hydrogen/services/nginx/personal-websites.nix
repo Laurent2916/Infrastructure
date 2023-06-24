e74505a986b428921772ba550f4378c1f5f46b21
Laureηt <laurent@fainsin.bzh>
2023-06-24 17:59:31 +0200

    ✨ (hydrogen/nginx) fetch personnal website from gitea pages branch

--- hosts/hydrogen/services/nginx/personal-websites.nix
+++ hosts/hydrogen/services/nginx/personal-websites.nix
@@ -1,21 +1,28 @@
-{ ... }: {
+{ pkgs, ... }:
+let
+  pages = pkgs.fetchFromGitea {
+    domain = "git.fainsin.bzh";
+    owner = "Laurent";
+    repo = "personal-website";
+    rev = "pages";
+    sha256 = "sha256-ahx9YSKyImQDu7k6/hbgYQdAnw5yInXQ/n14CLWTVkc=";
+  };
+in {
   services.nginx.virtualHosts = {
-
+    "laurent.fainsin.bzh" = {
+      enableACME = true;
+      forceSSL = true;
+      root = "${pages}";
+    };
     "fainsin.bzh" = {
       enableACME = true;
       forceSSL = true;
       locations."/".return =
         ''301 "$scheme://laurent.fainsin.bzh$request_uri"'';
     };
-    "laurent.fainsin.bzh" = {
-      enableACME = true;
-      forceSSL = true;
-      root = "/srv/www/";
-    };
     default = {
       default = true;
       locations."/".return = ''301 "$scheme://fainsin.bzh" '';
     };
-
   };
 }
