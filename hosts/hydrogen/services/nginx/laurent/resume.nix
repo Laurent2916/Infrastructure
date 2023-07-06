e7d81d6304152ff5965006c3cee227fd82a295d2
Laureηt <laurent@fainsin.bzh>
2023-07-06 19:34:20 +0200

    🐛 (hydrogen/nginx) fix a couple broken LFS sha256

--- hosts/hydrogen/services/nginx/laurent/resume.nix
+++ hosts/hydrogen/services/nginx/laurent/resume.nix
@@ -4,7 +4,7 @@ let
     url = "https://git.fainsin.bzh/Laurent/resume";
     fetchLFS = true; # https://github.com/go-gitea/gitea/issues/4773
     rev = "57134888a29bc56e4ca3a2507d4e3b728619948f"; # pages
-    sha256 = "0prm5j39dz5lp2bafqyh1mb7045h0csb4k42m92yy0m88pfk1m5x";
+    sha256 = "sha256-i4VwSFB5XoI5ySfGx4sCeZDWqAJ9p53OCy9ewlOCWFU=";
   };
 in {
   services.nginx.virtualHosts = {
