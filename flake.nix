127c5b163e026f6352172d13a1003d06090c111a
Laureηt <laurent@fainsin.bzh>
2023-08-12 20:23:45 +0200

    ✨ add nix-community cachix

--- flake.nix
+++ flake.nix
@@ -30,6 +30,15 @@
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
+  nixConfig = {
+    extra-substituters =
+      [ "https://nix-community.cachix.org" "https://hyprland.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
+
   outputs = { nixpkgs, flake-utils, lanzaboote, agenix, home-manager
     , nixos-hardware, ... }@attrs:
 
