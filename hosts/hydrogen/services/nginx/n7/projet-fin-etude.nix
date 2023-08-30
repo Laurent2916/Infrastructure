e970513d13a80c40c48c08c7475cfd2406bab7e7
Laureηt <laurent@fainsin.bzh>
2023-08-30 20:32:58 +0200

    ⬆️ nix flake update

--- hosts/hydrogen/services/nginx/n7/projet-fin-etude.nix
+++ hosts/hydrogen/services/nginx/n7/projet-fin-etude.nix
@@ -3,7 +3,7 @@ let
   pages = pkgs.fetchgit {
     url = "https://git.fainsin.bzh/ENSEEIHT/projet-fin-etude-rapport";
     rev = "193636d2f49e3b190606f1f8f70a9ebe29c7630e"; # pages
-    sha256 = "sha256-rLeX7M/oHK0p/OkkeqjWLAWF0tOMs8lJNC9EsRA7VF4=";
+    sha256 = "0pjl7c8b2i1g6i4wkcwcsg98a19cssl7l979zhlss778rzn9gdxc";
   };
 in {
   alias = "${pages}/";
