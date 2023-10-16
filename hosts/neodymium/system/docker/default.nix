540f7d07e9069b8edf234ec033583408cfed8fe7
Laureηt <laurent@fainsin.bzh>
2023-10-16 20:25:55 +0200

    🐋 (neodymium/system) fix podman config to enable podman config

--- hosts/neodymium/system/docker/default.nix
+++ hosts/neodymium/system/docker/default.nix
@@ -3,6 +3,9 @@
     enable = true;
 
     dockerCompat = true;
+    dockerSocket.enable = true;
+    defaultNetwork.settings.dns_enabled =
+      true; # required by networks in compose
     autoPrune.enable = true;
   };
 }
