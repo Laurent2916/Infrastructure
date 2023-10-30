4d0ccd65b9ec008d3aaeb9e7411666e6e65fe833
Laureηt <laurent@fainsin.bzh>
2023-10-30 20:38:25 +0100

    ⬆️ (hydrogen/nginx) update resume

--- hosts/hydrogen/services/nginx/laurent/resume.nix
+++ hosts/hydrogen/services/nginx/laurent/resume.nix
@@ -3,8 +3,8 @@ let
   pages = pkgs.fetchgit {
     url = "https://git.fainsin.bzh/Laurent/resume";
     fetchLFS = true; # https://github.com/go-gitea/gitea/issues/4773
-    rev = "49e55df24a9de37fb439f60140b677178c458bd3"; # pages
-    sha256 = "sha256-htb6jdeaZeoVUBXw5hiQ9K57cmw5qOE57vIGfxNLcPk=";
+    rev = "374ea752f30125aeab015a69086b3be183810ba3"; # pages
+    sha256 = "sha256-xaIOY486x2Ux/wnC4nGz6YLVFwmbCl1TZG47Lb7Yy0E=";
   };
 in {
   services.nginx.virtualHosts = {
