7da10c80f60800f8fabc042ba810eaeda7d2d609
Laureηt <laurent@fainsin.bzh>
2023-11-29 15:35:07 +0100

    ✨ (hydrogen/nginx) use disko for partitionning

---
+++ hosts/hydrogen/system/disko/default.nix
@@ -0,0 +1,40 @@
+{ ... }: {
+  boot.loader.grub = {
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+  disko.devices = {
+    disk.disk1 = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+          esp = {
+            end = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+  };
+}
