cc6d8f0545958c05080ed295830cefa71212ff26
Laureηt <laurent@fainsin.bzh>
2023-11-29 15:35:59 +0100

    🎨 (hydrogen/system) format nix settings file + enable experimental features

--- hosts/hydrogen/system/nix/default.nix
+++ hosts/hydrogen/system/nix/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ lib, pkgs, nixpkgs, ... }: {
   # optimizations
   nix.settings.auto-optimise-store = true;
 
@@ -8,4 +8,16 @@
     dates = "weekly";
     options = "--delete-older-than 30d";
   };
+
+  # experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # pin nixpkgs registry
+  nix.registry.nixpkgs.flake = nixpkgs;
+
+  # print diff between two generations
+  system.activationScripts.nvd-report-changes = ''
+    PATH=$PATH:${lib.makeBinPath [ pkgs.nvd pkgs.nix ]}
+    nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
+  '';
 }
