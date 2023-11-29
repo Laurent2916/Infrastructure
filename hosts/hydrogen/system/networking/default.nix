146076bad48f9d86451b4296d3e34504c75d7670
Laureηt <laurent@fainsin.bzh>
2023-11-29 15:35:23 +0100

    🎨 (hydrogen/services) format networking file

--- hosts/hydrogen/system/networking/default.nix
+++ hosts/hydrogen/system/networking/default.nix
@@ -1,10 +1,18 @@
 { ... }: {
   networking = {
+    # the name of the machine
     hostName = "hydrogen";
+
+    # the domain used for the machine
     domain = "fainsin.bzh";
+
+    # domain name servers, use clouflare family
+    networking.nameservers = [ "1.1.1.2" "1.0.0.2" ];
+
+    # TODO: bouger ça à côté des applications
     firewall = {
       allowedTCPPorts = [
-        22 # ssh
+        624 # ssh
         80 # http
         443 # https
       ];
