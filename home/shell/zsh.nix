ae15491e4727141989e12f13c57504353aa1424d
Laureηt <laurent@fainsin.bzh>
2023-12-31 14:33:16 +0100

    🔥 (home/shell) remove zsh-nix-shell plugin

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -23,17 +23,5 @@
       bindkey '^H' backward-kill-word
       bindkey '5~' kill-word
     '';
-    plugins = [
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "227d284ab2dc2f5153826974e0094a1990b1b5b9";
-          sha256 = "11mkq58ssafvkq8sq27v0dl19mi2myi392nhxgi1q2q9q0gazcaa";
-        };
-      }
-    ];
   };
 }
