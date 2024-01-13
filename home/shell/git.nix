9c4feaa22bed88ac9299371f64391fd6f89ced82
Laureηt <laurent@fainsin.bzh>
2024-01-13 14:51:19 +0100

    ✨ (home/shell) add some git config + parametrize git signing key according to hostname

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -1,12 +1,34 @@
-{...}: {
+{config, ...}: let
+  sign_key =
+    {
+      "silicium" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTvwXCT99s1EwOCeGQ28jyCAH/RBoLZza9k5I7wWdEu laurent@silicium";
+    }
+    ."${config.networking.hostName}";
+in {
   programs.git = {
     enable = true;
     lfs.enable = true;
+
     userName = "Laureηt";
-    userEmail = "laurent@fainsin.bzh";
+    userEmail = "laurent.git@fainsin.bzh";
+
     signing = {
       signByDefault = true;
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTvwXCT99s1EwOCeGQ28jyCAH/RBoLZza9k5I7wWdEu laurent@silicium";
+      key = sign_key;
+    };
+
+    ignores = [
+      ".cache"
+      ".DS_Store"
+      ".direnv"
+      ".venv"
+      "node_modules"
+      "result"
+      "result-*"
+    ];
+
+    extraConfig = {
+      merge.conflictstyle = "diff3";
     };
   };
 }
