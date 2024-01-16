bbfc136a3a3e47f85f22cfe8068b28e3225b838f
Laureηt <laurent@fainsin.bzh>
2024-01-16 18:09:20 +0100

    add todo signing key

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -1,6 +1,7 @@
 {osConfig, ...}: let
   sign_key =
     {
+      "aurum" = "TODO";
       "silicium" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTvwXCT99s1EwOCeGQ28jyCAH/RBoLZza9k5I7wWdEu laurent@silicium";
     }
     ."${osConfig.networking.hostName}";
