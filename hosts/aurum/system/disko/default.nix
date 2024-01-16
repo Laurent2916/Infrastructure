644a28a34b54be5b12d2f7d1207f68491a4fa52f
Laureηt <laurent@fainsin.bzh>
2024-01-16 17:41:23 +0100

    test

--- hosts/aurum/system/disko/default.nix
+++ hosts/aurum/system/disko/default.nix
@@ -8,7 +8,7 @@
           type = "gpt";
           partitions = {
             esp = {
-              size = "512MiB";
+              size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
