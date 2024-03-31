ec70981fd46dc463f9b00898e8c2daea540ea8e3
Laureηt <laurent.git@fainsin.bzh>
2024-03-31 17:28:55 +0200

    🔐 (cesium/system) add aurum key to ssh authorizedKeys

--- hosts/cesium/system/users/default.nix
+++ hosts/cesium/system/users/default.nix
@@ -1,6 +1,7 @@
 {...}: {
   users.mutableUsers = false;
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTvwXCT99s1EwOCeGQ28jyCAH/RBoLZza9k5I7wWdEu"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTvwXCT99s1EwOCeGQ28jyCAH/RBoLZza9k5I7wWdEu laurent@silicium"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItSJTtS7tO0Wz/WgHAFb3xuNFZpm8SOvr/o8uR83zzy laurent@aurum"
   ];
 }
