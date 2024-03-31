eeaf3a0bf0f05e15eca3026c3cabe32bb6f0548b
Laureηt <laurent.git@fainsin.bzh>
2024-03-31 16:25:41 +0200

    ⚡️ (silicium/services) use the right hyprland package (from the flake) inside greetd settings

--- hosts/silicium/services/greetd/default.nix
+++ hosts/silicium/services/greetd/default.nix
@@ -1,10 +1,16 @@
-{pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  hyprland = inputs.hyprland.packages."${pkgs.system}".hyprland;
+in {
   services.greetd = {
     enable = true;
 
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # TODO: use nix pkgs
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${hyprland}/bin/Hyprland";
         user = "greeter";
       };
     };
