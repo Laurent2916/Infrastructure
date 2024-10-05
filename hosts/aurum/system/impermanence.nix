a67ae07da3d6698ff3d79dbbf27074c2d24335eb
Laureηt <laurent.git@fainsin.bzh>
2024-10-05 23:33:23 +0200

    🐛 (aurum) add`/var/lib/fprint/` to impermanence persistence

--- hosts/aurum/system/impermanence.nix
+++ hosts/aurum/system/impermanence.nix
@@ -4,6 +4,7 @@
     directories = [
       "/var/log"
       "/var/lib/bluetooth"
+      "/var/lib/fprint/"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
