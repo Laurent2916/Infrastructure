1aec3f356ed1cb84afe34b15d50e3e8657270e61
Laureηt <laurent.git@fainsin.bzh>
2024-12-21 16:04:38 +0100

    (xenon) update networking config

--- hosts/xenon/system/networking.nix
+++ hosts/xenon/system/networking.nix
@@ -9,14 +9,14 @@
     # domain name servers, use clouflare family
     nameservers = ["1.1.1.2" "1.0.0.2"];
 
-    # firewall
-    firewall = {
-      enable = true;
-      # allowedTCPPorts = [
-      # 80 # http
-      # 443 # tls
-      # ];
-    };
+    # firewall, all ports closed by default
+    firewall.enable = true;
+
+    # Dynamic Host Configuration Protocol (DHCP)
+    useDHCP = true;
+
+    # disable IPv6, not used
+    enableIPv6 = false;
 
     # https://github.com/StevenBlack/hosts
     stevenblack.enable = true;
