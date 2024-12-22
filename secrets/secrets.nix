3abffe2c001e956d6dd5d684cf0f9077ab3d2caf
Laureηt <laurent.git@fainsin.bzh>
2024-12-22 13:01:26 +0100

    create xenon cloudflare tunnel age secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,9 @@
 let
   silicium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTvwXCT99s1EwOCeGQ28jyCAH/RBoLZza9k5I7wWdEu laurent@silicium"; # TODO: remove this
-  cesium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDVxpWbNJl+OXe6YImMpsJprfuTd+9UJVTiteiuyx6oP root@cesium";
+  aurum = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItSJTtS7tO0Wz/WgHAFb3xuNFZpm8SOvr/o8uR83zzy laurent@aurum";
+  laurent = [silicium aurum];
+
+  xenon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQIVwJ/EwChjp57My/v/tPhtcDCvdaRE2XZWUCMOOdk root@xenon";
 in {
-  "borgbackup.age".publicKeys = [silicium];
-  "gitea.age".publicKeys = [silicium cesium];
+  "xenon.cftunnel.age".publicKeys = laurent ++ [xenon];
 }
