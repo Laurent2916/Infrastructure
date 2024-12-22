770e53e4931a90bb3ba3780bd0ec540c6d77bf7a
Laureηt <laurent.git@fainsin.bzh>
2024-12-22 22:47:43 +0100

    (xenon) export ssh through cloudflare tunnel

--- hosts/xenon/services/ssh.nix
+++ hosts/xenon/services/ssh.nix
@@ -1,6 +1,17 @@
-{...}: {
+{
+  config,
+  builtins,
+  ...
+}: {
   services.openssh = {
     enable = true;
+    ports = [22];
     settings.PasswordAuthentication = false;
+  };
+
+  services.cloudflared.tunnels."xenon".ingress = {
+    "ssh.fainsin.bzh" = {
+      service = "ssh://localhost:${builtins.toString (builtins.elemAt config.services.openssh.ports 0)}";
+    };
   };
 }
