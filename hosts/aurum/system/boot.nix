10404ae5b3a4aa2155d96a62481f58fcd85dec85
Laureηt <laurent.git@fainsin.bzh>
2024-12-24 15:30:38 +0100

    (aurum/system): cleanup boot.nix and hardware.nix

--- hosts/aurum/system/boot.nix
+++ hosts/aurum/system/boot.nix
@@ -18,7 +18,7 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [""];
+  boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 }
