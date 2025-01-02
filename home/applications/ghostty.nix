1934785edf0ed3f4ed899b7120629d270ca500a7
Laureηt <laurent.git@fainsin.bzh>
2025-01-02 11:27:00 +0100

    (home) ghostty, use new home-manager module

--- home/applications/ghostty.nix
+++ home/applications/ghostty.nix
@@ -1,9 +1,13 @@
 {...}: {
-  xdg.configFile."ghostty/config".text = ''
-    font-family="FiraCode Nerd Font Mono"
-    gtk-single-instance=true
-    background-opacity=0.85
-    window-decoration=false
-    font-size=9.5
-  '';
+  programs.ghostty = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      font-family = "FiraCode Nerd Font Mono";
+      gtk-single-instance = true;
+      background-opacity = 0.85;
+      window-decoration = false;
+      font-size = 9.5;
+    };
+  };
 }
